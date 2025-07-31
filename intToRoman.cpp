3d73132a1bfe27262fd383b758114ae95186d3b3
Lucas Rech <lucasrech00@gmail.com>
2025-07-31 10:02:04 -0300

    add int to roman challenge

---
+++ intToRoman.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        int decimalNums[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string romanNums[13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        string concatRomanChars;
+        while(num > 0) {
+            for (int i = 0; i < 13; i++) {
+                if (num >= decimalNums[i]) {
+                    concatRomanChars += romanNums[i];
+                    num -= decimalNums[i];
+                    break;
+                }
+            }
+        }
+        return concatRomanChars;
+    }
+};
+
+int main() {
+    cout << Solution().intToRoman(3749) << endl;
+    return 0;
+}
